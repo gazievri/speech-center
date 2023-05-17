55efe5996fd37849a36bdae50c4a3c6784044da5
Vlad B <belousov7vlad@gmail.com>
2023-05-17 22:30:12 +0300

    how to find spec

---
+++ src/components/HowToFind/HowToFind.jsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.sass'
+import { howToFindData } from '../../data/data'
+
+
+export const HowToFind = () => {
+
+    return (
+        <section className={styles.howToFind}>
+            <h2 className={styles.title}>Как найти хорошего специалиста по речи:</h2>
+            <article className={styles.wrapper}>
+
+                <ul className={styles.list}>
+
+                {howToFindData[0].map(el => <li className={styles.item}> <div className={styles.element__iconCheck}/> <p>{el}</p></li>)}
+                    
+                </ul>
+
+                <ul className={styles.list}>
+                {howToFindData[1].map(el => <li className={styles.item}> <div className={styles.element__iconCross}/> <p>{el}</p></li>)}
+                </ul>
+
+
+
+            </article>
+
+
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Hero } from '../components/Hero/Hero';
+import { HowToFind } from '../components/HowToFind/HowToFind';
 
 export const Home = () => {
   return (
     <>
       <Hero />
+      <HowToFind />
     </>
   );
 };
