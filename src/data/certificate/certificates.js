271cd608d30a53173380f9e33c934a48e24c2793
Ruslan Gaziev <grinrus@yandex.ru>
2023-05-23 15:01:17 +0300

    Добавлены картинки сертификатов

---
+++ src/data/certificate/certificates.js
@@ -0,0 +1,15 @@
+import certificate1 from './images/certificate-1-min.webp';
+import certificate2 from './images/certificate-2-min.webp';
+import certificate3 from './images/certificate-3-min.webp';
+import certificate4 from './images/certificate-4-min.webp';
+import certificate5 from './images/certificate-5-min.webp';
+import certificate6 from './images/certificate-6-min.webp';
+
+export const CERTIFICATES_DATA = [
+  certificate1,
+  certificate2,
+  certificate3,
+  certificate4,
+  certificate5,
+  certificate6,
+];
