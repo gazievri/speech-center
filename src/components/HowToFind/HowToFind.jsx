964f1eabea21ca1d7690fd2b96c729e0aebbb73c
Vlad B <belousov7vlad@gmail.com>
2023-05-24 12:42:01 +0300

    how to find adaptive

--- src/components/HowToFind/HowToFind.jsx
+++ src/components/HowToFind/HowToFind.jsx
@@ -1,31 +1,38 @@
-import styles from './styles.module.sass'
-import { howToFindData } from '../../data/data'
-
+import styles from "./styles.module.sass";
+import { howToFindData } from "../../data/data";
+import useMediaQuery from "../../utils/useMediaQuery";
+import { Slider } from "./Slider";
 
 export const HowToFind = () => {
 
-    return (
-        <section className={styles.howToFind} id='tips'>
+    const isMobile = useMediaQuery("(max-width: 800px)");
+  return (
+    <section className={styles.howToFind} id="tips">
+      <div className={styles.howToFindWrapper}>
+        <h2 className={styles.title}>
+          Как найти хорошего специалиста по речи:
+        </h2>
+        <article className={styles.wrapper}>
+            {!isMobile ?<ul className={styles.list}>
+            {howToFindData[0].map((el, index) => (
+              <li className={styles.item} key={index}>
+                {" "}
+                <div className={styles.element__iconCheck} /> <p >{el}</p>
+              </li>
+            ))}
+          </ul> : <Slider data={howToFindData[0]}/> }
+          
 
-            <div className={styles.howToFindWrapper}>
-            <h2 className={styles.title}>Как найти хорошего специалиста по речи:</h2>
-            <article className={styles.wrapper}>
-
-                <ul className={styles.list}>
-
-                {howToFindData[0].map((el, index) => <li className={styles.item} key={index}> <div className={styles.element__iconCheck}/> <p>{el}</p></li>)}
-                    
-                </ul>
-
-                <ul className={styles.list}>
-                {howToFindData[1].map((el, index) => <li className={styles.item}key={index}> <div className={styles.element__iconCross}/> <p>{el}</p></li>)}
-                </ul>
-
-
-
-            </article>
-
-            </div>
-        </section>
-    )
-}
+          {!isMobile ?<ul className={styles.list}>
+            {howToFindData[1].map((el, index) => (
+              <li className={styles.item} key={index}>
+                {" "}
+                <div className={styles.element__iconCross} /> <p>{el}</p>
+              </li>
+            ))}
+          </ul> : <Slider data={howToFindData[1]} cross/> }
+        </article>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/HowToFind/Slider.jsx
@@ -0,0 +1,30 @@
+import styles from "./styles.module.sass";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/scss";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
+
+
+export const Slider = ({ data, cross=false }) => {
+  return (
+    <div className={styles.sliderWrapper}>
+      <Swiper
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className={styles.sliderContainer}
+        //   rewind={true}
+      >
+        {data.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className={styles.sliderItem} key={index}>
+              <div className={cross ? styles.element__iconCross : styles.element__iconCheck}/><p className={styles.sliderText}>{item}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
