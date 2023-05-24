cbdc06a6e80ffeabaf8d24f5a826fbf2be4e1355
Ruslan Gaziev <grinrus@yandex.ru>
2023-05-25 01:13:25 +0300

    Фиксированный header на мобилке и кнопка вверх

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './styles/@globals.sass';
 import { Header } from './layout/Header/Header';
 import { Main } from './layout/Main/Main';
 import { Footer } from './layout/Footer/Footer';
+import { ToTop } from './components/ToTop/ToTop';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <ToTop />
     </div>
   );
 }
