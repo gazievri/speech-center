40873905d5065d732f72530f0551150a3dbf9f2e
Ruslan Gaziev <grinrus@yandex.ru>
2023-05-24 12:41:35 +0300

    Hero мобильная адаптация

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,42 +2,62 @@ import styles from './styles.module.sass';
 import { ButtonW } from '../ButtonW/Button';
 import { Link } from 'react-router-dom';
 import heroImage from '../../assets/images/hero.png';
+import useMediaQuery from '../../utils/useMediaQuery';
 
 export const Hero = () => {
+  const isTablet = useMediaQuery('(max-width: 850px)');
+
   return (
     <section className={styles.hero} id="hero">
       <div>
         <ul className={styles.services}>
           <li className={styles.service}>
             <h2 className={styles.serviceText}>Заикаетесь?</h2>
-            <Link className={styles.link}>подробнее</Link>
+            <Link className={styles.link}>Подробнее</Link>
           </li>
           <li className={styles.service}>
             <h2 className={styles.serviceText}>
               Не понимают, что вы говорите?
             </h2>
-            <Link className={styles.link}>подробнее</Link>
+            <Link className={styles.link}>Подробнее</Link>
           </li>
           <li className={styles.service}>
             <h2 className={styles.serviceText}>Ребенок молчит у доски?</h2>
-            <Link className={styles.link}>подробнее</Link>
+            <Link className={styles.link}>Подробнее</Link>
           </li>
           <li className={styles.service}>
             <h2 className={styles.serviceText}>
-              Дизартрия, дислексия, дислалия, ОНР&nbsp;(3 уровня), афазия, ДЦП, ФФНР?
+              Дизартрия, дислексия, дислалия, ОНР&nbsp;(3 уровня), афазия, ДЦП,
+              ФФНР?
             </h2>
-            <Link className={styles.link}>подробнее</Link>
+            <Link className={styles.link}>Подробнее</Link>
           </li>
         </ul>
-        <h2 className={styles.moto}>Мы вам поможем</h2>
-        <ButtonW />
+
+        {!isTablet && (
+          <>
+            <h2 className={styles.moto}>Мы вам поможем</h2>
+            <div className={styles.btnWrapper}>
+              <ButtonW />
+            </div>
+          </>
+        )}
       </div>
 
-      <div>
+      
         <div className={styles.imgBack}>
           <img src={heroImage} alt="" className={styles.img} />
         </div>
-      </div>
+      
+
+      {isTablet && (
+        <>
+          <h2 className={styles.moto}>Мы вам поможем</h2>
+          <div className={styles.btnWrapper}>
+            <ButtonW />
+          </div>
+        </>
+      )}
     </section>
   );
 };
