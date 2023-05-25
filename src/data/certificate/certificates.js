9ce178b3ca7cbf8ee87c66e08f44910af113efb4
Ruslan Gaziev <grinrus@yandex.ru>
2023-05-25 20:53:57 +0300

    Исправлено качество логотипа и галочек/крестов

--- src/data/certificate/certificates.js
+++ src/data/certificate/certificates.js
@@ -1,6 +1,6 @@
 import certificate1 from './images/certificate-1-min.webp';
 import certificate2 from './images/certificate-2-min.webp';
-import certificate3 from './images/certificate-3-min.webp';
+// import certificate3 from './images/certificate-3-min.webp';
 import certificate4 from './images/certificate-4-min.webp';
 import certificate5 from './images/certificate-5-min.webp';
 import certificate6 from './images/certificate-6-min.webp';
